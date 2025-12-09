20c4955926110a74e53254414f68f682df7e0ac6
Jarek Growin <jg.growin@gmail.com>
2025-12-09 14:57:36 +0100

    update tos i privacy

--- plugin.rb
+++ plugin.rb
@@ -2,13 +2,13 @@
 
 # name: discourse-translate-system-posts
 # about: Enables AI translation for bot and system user posts
-# version: 0.7.0
+# version: 0.8.0
 # authors: Jarek
 # url: https://github.com/growandwin/discourse-translate-system-posts
 
 after_initialize do
   reloadable_patch do |plugin|
-    # Redefine the class - this replaces the existing methods
+    # Patch PostCandidates - remove user_id > 0 restriction
     DiscourseAi::Translation::PostCandidates.class_eval do
       private_class_method def self.get
         posts =
@@ -34,6 +34,7 @@ after_initialize do
       end
     end
 
+    # Patch TopicCandidates - remove user_id > 0 restriction
     DiscourseAi::Translation::TopicCandidates.class_eval do
       private_class_method def self.get
         topics =
@@ -53,6 +54,43 @@ after_initialize do
               Archetype.private_message,
             )
         end
+      end
+    end
+
+    # Patch StaticController - use localized content for /privacy, /tos, /faq
+    StaticController.class_eval do
+      alias_method :original_show, :show
+
+      def show
+        map = StaticController::DEFAULT_PAGES.deep_merge(StaticController::CUSTOM_PAGES)
+        page = params[:id]
+        page = page.gsub(/[^a-z0-9\_\-]/, "")
+
+        if map.has_key?(page) && SiteSetting.content_localization_enabled
+          topic_id_setting = map[page][:topic_id]
+          if topic_id_setting
+            topic = Topic.find_by_id(SiteSetting.get(topic_id_setting))
+            if topic
+              post = topic.posts.first
+              user_locale = I18n.locale.to_s
+              
+              # Try exact match first, then base locale
+              localization = PostLocalization.find_by(post_id: post.id, locale: user_locale)
+              localization ||= PostLocalization.find_by(post_id: post.id, locale: user_locale.split("_").first)
+              localization ||= PostLocalization.where(post_id: post.id)
+                                .where("locale LIKE ?", "#{user_locale.split('_').first}%")
+                                .first
+
+              if localization
+                original_show
+                @body = localization.cooked if @body.present?
+                return
+              end
+            end
+          end
+        end
+
+        original_show
       end
     end
   end
